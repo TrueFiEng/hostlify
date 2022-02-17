3f9062867ec08a78154570ed2008c7ac2cb01294
mateusz <mistrzumati@spoko.pl>
2022-02-17 10:13:48 +0100

    Complete server upload logic and set up dockerfile

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import fastify from 'fastify'
 import fileUpload from 'fastify-file-upload'
 import fs from 'fs'
+import { exec } from 'child_process'
 import { UploadRequest } from './types'
-import { PORT } from './constants'
+import { PORT, SERVER_TEMPLATE } from './constants'
 
 const server = fastify()
 server.register(fileUpload)
@@ -10,26 +11,22 @@ server.register(fileUpload)
 server.post<UploadRequest>('/upload/:id', async (request, reply) => {
 
     const { id } = request.params
-    const repositoryPath = './repository/' + id
+    const repositoryPath = `./repository/${id}`
+    const configPath = `./config/serverConfigs/${id}.conf`
 
-    fs.mkdir(repositoryPath, { recursive: true }, function(err) {
-        if(err ) {
-            console.log(err)
-            return reply.status(400).send("done")
-        }
-    })
+    const status = fs.mkdirSync(repositoryPath, { recursive: true })
+
+    const serverConfig = SERVER_TEMPLATE.replace(/{{serverName}}/g, id)
+    fs.writeFileSync(configPath, serverConfig)
 
     for (const [key, file] of Object.entries(request.body)) {
-        const filePath = repositoryPath + '/' + file.name
-        fs.appendFile(filePath, file.data, (err) => {
-            if(err) {
-                console.log(err)
-                return reply.status(400).send("cannot create file " + key)
-            }
-        })
+        const filePath = `${repositoryPath}/${file.name}`
+        fs.writeFileSync(filePath, file.data)
     }
+    exec('docker compose up -d --force-recreate --no-deps --build server1')
 
-    return reply.status(200).send("done")
+    const url = `${id}.lvh.me`
+    return reply.status(201).send({url})
 })
 
 server.listen(PORT, (err, address) => {
